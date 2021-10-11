a06a6defac7ab7210d06b2779eb2fa1b90261d18
Ann-iOS <m18620345206@163.com>
2021-10-11 10:10:47 +0800

    add gitignore

--- Example/Pods/Target Support Files/Pods-Sm2FrameworkDemo-Sm2FrameworkDemoUITests/Pods-Sm2FrameworkDemo-Sm2FrameworkDemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Sm2FrameworkDemo_Sm2FrameworkDemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Sm2FrameworkDemo_Sm2FrameworkDemoUITests
-@end
